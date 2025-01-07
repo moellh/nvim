ea8cb9ad9c9a476e47fef7531a544fb5ec8e14d8
Henrik Möllmann <henmoell@gmail.com>
2025-01-07 12:26:53 +0100

    Added todo-comments options

--- lua/moellh/custom/other_plugins.lua
+++ lua/moellh/custom/other_plugins.lua
@@ -1,17 +1,91 @@
 return {
 
     {
-        'nvim-tree/nvim-web-devicons',
+        "nvim-tree/nvim-web-devicons",
         lazy = true,
     },
 
-    'fladson/vim-kitty',
+    "fladson/vim-kitty",
 
     -- Highlight todo, notes, etc in comments
-    { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            signs = true, -- show icons in the signs column
+            sign_priority = 8, -- sign priority
+            -- keywords recognized as todo comments
+            keywords = {
+                FIX = {
+                    icon = " ", -- icon used for the sign, and in search results
+                    color = "error", -- can be a hex color, or a named color (see below)
+                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                    -- signs = false, -- configure signs for some keywords individually
+                },
+                TODO = { icon = " ", color = "info" },
+                HACK = { icon = " ", color = "warning" },
+                WARN = {
+                    icon = " ",
+                    color = "warning",
+                    alt = { "WARNING", "XXX" },
+                },
+                PERF = {
+                    icon = " ",
+                    alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" },
+                },
+                NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                TEST = {
+                    icon = "⏲ ",
+                    color = "test",
+                    alt = { "TESTING", "PASSED", "FAILED" },
+                },
+            },
+            gui_style = {
+                fg = "NONE", -- The gui style to use for the fg highlight group.
+                bg = "BOLD", -- The gui style to use for the bg highlight group.
+            },
+            merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+            -- highlighting of the line containing the todo comment
+            -- * before: highlights before the keyword (typically comment characters)
+            -- * keyword: highlights of the keyword
+            -- * after: highlights after the keyword (todo text)
+            highlight = {
+                multiline = true, -- enable multine todo comments
+                multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+                multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+                before = "", -- "fg" or "bg" or empty
+                keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                after = "fg", -- "fg" or "bg" or empty
+                pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+                comments_only = false, -- uses treesitter to match keywords in comments only
+                max_line_len = 400, -- ignore lines longer than this
+                exclude = {}, -- list of file types to exclude highlighting
+            },
+            -- list of named colors where we try to extract the guifg from the
+            -- list of highlight groups or use the hex color if hl not found as a fallback
+            colors = {
+                error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+                warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+                info = { "DiagnosticInfo", "#2563EB" },
+                hint = { "DiagnosticHint", "#10B981" },
+                default = { "Identifier", "#7C3AED" },
+                test = { "Identifier", "#FF00FF" },
+            },
+            search = {
+                command = "rg",
+                args = {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                },
+            },
+        },
+    },
 
     { -- Collection of various small independent plugins/modules
-        'echasnovski/mini.nvim',
+        "echasnovski/mini.nvim",
         config = function()
             -- Better Around/Inside textobjects
             --
@@ -19,7 +93,7 @@ return {
             --  - va)  - [V]isually select [A]round [)]parenthen
             --  - yinq - [Y]ank [I]nside [N]ext [']quote
             --  - ci'  - [C]hange [I]nside [']quote
-            require('mini.ai').setup { n_lines = 500 }
+            require("mini.ai").setup { n_lines = 500 }
 
             -- Add/delete/replace surroundings (brackets, quotes, etc.)
             --
@@ -33,5 +107,5 @@ return {
         end,
     },
 
-    'nvim-neotest/nvim-nio',
+    "nvim-neotest/nvim-nio",
 }
