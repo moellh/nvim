6534db3832fa7f46bbe65e9b64446dc30a8773a9
Henrik Möllmann <henmoell@gmail.com>
2025-01-12 14:36:16 +0100

    Added Oil shortcut

--- lua/moellh/custom/files.lua
+++ lua/moellh/custom/files.lua
@@ -3,6 +3,8 @@ vim.g.netrw_banner = 0
 -- Make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
 return {
 
     {
