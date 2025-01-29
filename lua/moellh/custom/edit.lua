b553c4e00ad97338c014d82dcb5adbc816167828
Henrik Möllmann <henmoell@gmail.com>
2025-01-29 14:04:57 +0100

    Changed avante setup

--- lua/moellh/custom/edit.lua
+++ lua/moellh/custom/edit.lua
@@ -110,65 +110,6 @@ return {
     },
 
     {
-        "yetone/avante.nvim",
-
-        event = "VeryLazy",
-        lazy = false,
-        version = false, -- set this if you want to always pull the latest change
-
-        opts = {
-            provider = "copilot",
-            auto_suggestions_provider = "copilot",
-            copilot = {
-                max_tokens = 4096,
-            },
-            hints = { enabled = false },
-        },
-
-        -- `make BUILD_FROM_SOURCE=true` to build from source
-        build = "make",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "stevearc/dressing.nvim",
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
-            {
-                -- support for image pasting
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- recommended settings
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        -- required for Windows users
-                        use_absolute_path = true,
-                    },
-                },
-            },
-        },
-    },
-
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-            require("copilot").setup {
-                suggestion = {
-                    auto_trigger = true,
-                },
-            }
-        end,
-    },
-
-    {
         "danymat/neogen",
         config = true,
     },
