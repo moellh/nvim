29450abf6d0a86a7331c0233e67d0debe2f38683
Henrik Möllmann <henmoell@gmail.com>
2025-05-04 02:18:09 +0200

    Custom Catppuccin theme with black background

--- lua/moellh/custom/looks.lua
+++ lua/moellh/custom/looks.lua
@@ -44,45 +44,79 @@ return {
         config = function()
             -- manually select colorscheme based on kitty theme
             -- otherwise flickering on startup of Catppuccin-Latte theme
-            local kitty_theme = vim.fn.system [[\
-                grep '# name' ~/.config/kitty/current-theme.conf | \
-                awk 'BEGIN { ORS = "" } {print $3}'
-            ]]
-            if kitty_theme == "Catppuccin-Latte" then
-                vim.cmd.colorscheme "catppuccin-latte"
-            else
-                vim.cmd.colorscheme "catppuccin-mocha"
-            end
+            -- local kitty_theme = vim.fn.system [[\
+            --     grep '# name' ~/.config/kitty/current-theme.conf | \
+            --     awk 'BEGIN { ORS = "" } {print $3}'
+            -- ]]
+            -- if kitty_theme == "Catppuccin-Latte" then
+            --     vim.cmd.colorscheme "catppuccin-latte"
+            -- else
+            --     vim.cmd.colorscheme "catppuccin-mocha"
+            -- end
 
             -- choose this colorscheme
             require("catppuccin").setup {
-                latte = {
-                    rosewater = "#cc7983",
-                    flamingo = "#bb5d60",
-                    pink = "#d54597",
-                    mauve = "#a65fd5",
-                    red = "#b7242f",
-                    maroon = "#db3e68",
-                    peach = "#e46f2a",
-                    yellow = "#bc8705",
-                    green = "#1a8e32",
-                    teal = "#00a390",
-                    sky = "#089ec0",
-                    sapphire = "#0ea0a0",
-                    blue = "#017bca",
-                    lavender = "#8584f7",
-                    text = "#444444",
-                    subtext1 = "#555555",
-                    subtext0 = "#666666",
-                    overlay2 = "#777777",
-                    overlay1 = "#888888",
-                    overlay0 = "#999999",
-                    surface2 = "#aaaaaa",
-                    surface1 = "#bbbbbb",
-                    surface0 = "#cccccc",
-                    base = "#ffffff",
-                    mantle = "#eeeeee",
-                    crust = "#dddddd",
+                background = { -- :h background
+                    light = "latte",
+                    dark = "mocha",
+                },
+                color_overrides = {
+                    mocha = {
+                        rosewater = "#f5e0dc",
+                        flamingo = "#f2cdcd",
+                        pink = "#f5c2e7",
+                        mauve = "#cba6f7",
+                        red = "#f38ba8",
+                        maroon = "#eba0ac",
+                        peach = "#fab387",
+                        yellow = "#f9e2af",
+                        green = "#a6e3a1",
+                        teal = "#94e2d5",
+                        sky = "#89dceb",
+                        sapphire = "#74c7ec",
+                        blue = "#89b4fa",
+                        lavender = "#b4befe",
+                        text = "#cdd6f4",
+                        subtext1 = "#bac2de",
+                        subtext0 = "#a6adc8",
+                        overlay2 = "#9399b2",
+                        overlay1 = "#7f849c",
+                        overlay0 = "#6c7086",
+                        surface2 = "#585b70",
+                        surface1 = "#45475a",
+                        surface0 = "#313244",
+                        base = "#000000", -- "#1e1e2e",
+                        mantle = "#000000",
+                        crust = "#000000",
+                    },
+                    latte = {
+                        rosewater = "#cc7983",
+                        flamingo = "#bb5d60",
+                        pink = "#d54597",
+                        mauve = "#a65fd5",
+                        red = "#b7242f",
+                        maroon = "#db3e68",
+                        peach = "#e46f2a",
+                        yellow = "#bc8705",
+                        green = "#1a8e32",
+                        teal = "#00a390",
+                        sky = "#089ec0",
+                        sapphire = "#0ea0a0",
+                        blue = "#017bca",
+                        lavender = "#8584f7",
+                        text = "#444444",
+                        subtext1 = "#555555",
+                        subtext0 = "#666666",
+                        overlay2 = "#777777",
+                        overlay1 = "#888888",
+                        overlay0 = "#999999",
+                        surface2 = "#aaaaaa",
+                        surface1 = "#bbbbbb",
+                        surface0 = "#cccccc",
+                        base = "#ffffff",
+                        mantle = "#eeeeee",
+                        crust = "#dddddd",
+                    },
                 },
             }
         end,
@@ -177,7 +211,8 @@ return {
                     },
                     ignore_focus = {},
                     always_divide_middle = true,
-                    globalstatus = false,
+                    always_show_tabline = true,
+                    globalstatus = true,
                     refresh = {
                         statusline = 100,
                         tabline = 100,
